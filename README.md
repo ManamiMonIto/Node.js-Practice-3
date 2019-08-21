23543968bee70f36bafdd73e4a40b6f6f88aa5f7
Manami Ito <manami.j27@gmail.com>
2019-08-21 21:49:39 +0200

    create table migration and seed

---
+++ README.md
@@ -0,0 +1,4 @@
+#CROUD sticker
+
+##ERD
+![](https://www.lucidchart.com/publicSegments/view/dc3296b8-ca20-4f50-9d00-84385a93483d/image.png)
---
+++ stickers.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    title: 'Newbie',
+    description: 'became thin guess cook visitor',
+    rating: 0,
+    url: 'https://octodex.github.com/images/surftocat.png',
+  },
+  {
+    title: 'Newbie',
+    description: 'became thin guess cook visitor',
+    rating: 0,
+    url: 'https://octodex.github.com/images/surftocat.png',
+  },
+  {
+    title: 'Newbie',
+    description: 'became thin guess cook visitor',
+    rating: 0,
+    url: 'https://octodex.github.com/images/surftocat.png',
+  },
+  {
+    title: 'Newbie',
+    description: 'became thin guess cook visitor',
+    rating: 0,
+    url: 'https://octodex.github.com/images/surftocat.png',
+  }
+]
